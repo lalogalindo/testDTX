41099b5547ff7d68f9abde16c005b4d1bc7bfc71
Eduardo Galindo <egalindo@mercsoft.mx>
2020-07-29 22:19:42 -0500

    Movie Card and Footer Mokup

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+    <nav class="footer__navigation">
+        <div class="footer__logo">
+            @svg('img/logoBig.svg', 'footer-logo')
+            <a class="rounded" href="/signup">Join the Community</a>
+        </div>
+        <div class="footer__menu">
+            <h3 class="footer__menu--title">The Basics</h3>
+            <ul>
+                <li class="footer__menu--item"><a href="/about">About TMDb</a></li>
+                <li class="footer__menu--item"><a href="/about/staying-in-touch">Contact Us</a></li>
+                <li class="footer__menu--item"><a href="/talk">Support Forums</a></li>
+                <li class="footer__menu--item"><a href="/documentation/api">API</a></li>
+                <li class="footer__menu--item"><a href="https://status.themoviedb.org/" target="_blank" rel="noopener">System Status</a></li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="footer__menu--title">Get Involved</h3>
+            <ul>
+                <li class="footer__menu--item"><a href="/bible"><span class="glyphicons glyphicons-asterisk"></span> Contribution Bible</a></li>
+                <li class="footer__menu--item"><a href="/apps">3rd Party Applications</a></li>
+                <li class="footer__menu--item"><a href="/movie/new">Add New Movie</a></li>
+                <li class="footer__menu--item"><a href="/tv/new">Add New TV Show</a></li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="footer__menu--title">Community</h3>
+            <ul>
+                <li class="footer__menu--item"><a href="/documentation/community/guidelines">Guidelines</a></li>
+                <li class="footer__menu--item"><a href="/discuss">Discussions</a></li>
+                <li class="footer__menu--item"><a href="/leaderboard">Leaderboard</a></li>
+                <li class="footer__menu--item"><a href="https://twitter.com/themoviedb" target="_blank" rel="noopener">Twitter</a></li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="footer__menu--title">Legal</h3>
+            <ul>
+                <li class="footer__menu--item"><a href="/documentation/website/terms-of-use">Terms of Use</a></li>
+                <li class="footer__menu--item"><a href="/documentation/api/terms-of-use">API Terms of Use</a></li>
+                <li class="footer__menu--item"><a href="/privacy-policy">Privacy Policy</a></li>
+            </ul>
+        </div>
+    </nav>
+</footer>
